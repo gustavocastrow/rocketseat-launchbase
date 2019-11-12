60c78ad23299941f599da9928f965fc581e1a518
Gustavo Castro <gustavocastrocs@gmail.com>
2019-11-12 06:35:29 -0300

    Array e Objetos

---
+++ aulajs/array.js
@@ -0,0 +1,27 @@
+const alunos = [
+  {//0
+    nome: "Gustavo",
+    nota: 9.8
+  },
+
+  {//1
+    nome: "Diego",
+    nota: 10
+  },
+
+  {//2
+    nome: "João",
+    nota: 2
+  }
+
+]
+
+console.log(alunos);
+
+const nomesDeAlunos = ["Gustavo", "Diego", "João"]
+console.log(nomesDeAlunos[1]);
+
+
+
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+console.log(media);
