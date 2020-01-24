881a4874c65737f77a208c35055de8fcf98985b0
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-24 06:20:38 -0300

    Objetos e Arrays

---
+++ Introdução à programação/04-objetos/index.js
@@ -0,0 +1,18 @@
+const aluno01 = {
+  nome: 'Gustavo',
+  nota: 9.8
+}
+
+const aluno02 = {
+  nome: 'Pedro',
+  nota: 7
+}
+
+const aluno03 = {
+  nome: 'Rafael',
+  nota: 6
+}
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3;
+
+console.log(media)
