b01e84fca49ccdc569d13c7a794d06f57f159ff1
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-27 10:47:29 -0300

    Template Engine

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
-server.get('/', function(req, res){
-  return res.send('Hello Omnistack!')
+server.use(express.static('public'))
 
+server.set('view engine', 'html')
+nunjucks.configure('views', {
+  express: server
+})
+
+server.get('/', function(req, res){
+  return res.render('about')
+
+})
+
+server.get('/portfolio', function(req, res){
+  return res.render('portfolio')
 })
 
 server.listen(5000, function(){
