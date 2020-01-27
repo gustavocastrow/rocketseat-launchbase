43bac4010ab95eddf1c90996f3bc7d15e29b07a5
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-27 12:33:12 -0300

    Dados dinâmicos

---
+++ backend/data.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57min",
+    price: "Free"
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de Áudio com Javascript | Mayk Brito",
+    duration: "1h45min",
+    price: "Free"
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "O que é API? Rest e Restful? | Mayk Brito",
+    duration: "33min",
+    price: "Free"
+  },
+  {
+    id: "rAzHvYnQ8DY",
+    title: "Array: High order functions | Mayk Brito",
+    duration: "54min",
+    price: "Free"
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desvendando o CSS Grid na prática | Mayk Brito",
+    duration: "36min",
+    price: "Free"
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a variável this | Mayk Brito",
+    duration: "48min",
+    price: "Free"
+  },
+
+]
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require('./data');
 
 server.use(express.static('public'))
 
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 nunjucks.configure('views', {
   express: server
 })
@@ -16,7 +17,8 @@ server.get('/', function(req, res){
 })
 
 server.get('/portfolio', function(req, res){
-  return res.render('portfolio')
+
+  return res.render('portfolio', {items: videos})
 })
 
 server.listen(5000, function(){
