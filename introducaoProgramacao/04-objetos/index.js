f5d0b828564419781d035c375c015ee9a8240e31
gustavocastro <gustavocastrocs@gmail.com>
2020-01-28 19:24:12 -0300

    Objetos

--- introducaoProgramacao/04-objetos/index.js
+++ introducaoProgramacao/04-objetos/index.js
@@ -1,18 +1,30 @@
+//Crie um programa que calcuka a média das notas entre os alunos e envia
+//mensagem do cálculo da média.
+
+
 const aluno01 = {
-  nome: 'Gustavo',
+  nome: "Mayk",
   nota: 9.8
 }
 
 const aluno02 = {
-  nome: 'Pedro',
-  nota: 7
+  nome: "Diego",
+  nota: 10
 }
 
 const aluno03 = {
-  nome: 'Rafael',
-  nota: 6
+  nome: "Fulano",
+  nota: 2
 }
 
-const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3;
 
-console.log(media)
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+
+//Se a média for maior que 5, parabenizar a turma
+
+if (media > 5) {
+  console.log(`A média foi de ${media}. Parabéns!`)
+} else {
+  console.log(`A média foi menor que 5`)
+}
