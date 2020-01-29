6dd6a2140986f937389176d4736dd2e9d72a8960
gustavocastro <gustavocastrocs@gmail.com>
2020-01-29 18:49:23 -0300

    For

--- introducaoProgramacao/07-estruturaRepeticao/index.js
+++ introducaoProgramacao/07-estruturaRepeticao/index.js
@@ -1,69 +1,64 @@
-// Criar um programa que calcula a média das turmas de alunos e envia mensagem do
-// calculo da média
+//Criar um programa que calcula a média das turmas de alunos e envia
+//mesagem do cálculo da média
+
 
 const alunosDaTurmaA = [
   {
     nome: 'Gustavo',
-    nota: 7.2
+    nota: 9.8
   },
   {
-    nome: 'Pedro',
-    nota: 5
+    nome: 'Diego',
+    nota: 7.2,
   },
   {
     nome: 'Mayk',
-    nota: 10
-  },
-  {
-    nome: 'Vito',
-    nota: 2
+    nota: 3.2
   }
 ]
 
 const alunosDaTurmaB = [
   {
-    nome: 'Marcela',
-    nota: 6
+    nome: 'Gabriel',
+    nota: 3.8
   },
   {
-    nome: 'Fernanda',
-    nota: 2
+    nome: 'Denis',
+    nota: 2.2,
   },
   {
-    nome: 'Julia',
-    nota: 3
+    nome: 'Rafael',
+    nota: 3.2
   },
   {
-    nome: 'Ana',
-    nota: 0
+    nome: 'Joao',
+    nota: 1.0
   }
 ]
 
-function calculaMedia(alunos){
+function calculaMedia(alunos) {
   let soma = 0;
   for (let i = 0; i < alunos.length; i++){
     soma = soma + alunos[i].nota
     
   }
-
-  const media = soma / alunos.length;
-  return media
-
+  const media = soma / alunos.length
+  return media 
 }
 
-const media1 = calculaMedia(alunosDaTurmaA);
-const media2 = calculaMedia(alunosDaTurmaB);
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
 
 
 function enviaMensagem(media, turma){
-  
-  if (media > 5) {
-    console.log(`A média da turma ${turma} foi de ${media}. Parabéns!!`)
-  } else {
-    console.log(`A média da turma ${turma} foi de ${media}, portanto REPROVADO!!!`)
+  if(media > 5) {
+    console.log(`A média da ${turma} foi de ${media}. Parábens`)
+  }else {
+    console.log(`A média da ${turma} é de ${media} menor que 5`)
   }
-
+  
 }
 
-enviaMensagem(media1, 'turmaA');
-enviaMensagem(media2, 'turmaB');
+enviaMensagem(media1, 'turmaA')
+enviaMensagem(media2, 'turmaB')
--- introducaoProgramacao/desafio02/vetoresObjetos/indexv2.js
+++ introducaoProgramacao/desafio02/vetoresObjetos/indexv2.js
@@ -8,9 +8,7 @@ const programador = {
   ]
 }
 
-// Imprima em tela o nome e especialidade da primeira tecnologia que o usuário utiliza no seguinte formato:
 
-// O usuário Carlos tem 32 anos e usa a tecnologia C++ com especialidade em Desktop
 
 console.log(`O usuário ${programador.nome} tem ${programador.idade} anos
             e usa a tecnologia ${programador.techs[0].nome} com especialidade em ${programador.techs[0].especialidade}`)
