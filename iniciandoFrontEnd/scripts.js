fc4f7fd8f80194506fb312e02cf251eae7a0d25e
gustavocastro <gustavocastrocs@gmail.com>
2020-02-01 20:08:42 -0300

    Manipulando o DOM

---
+++ iniciandoFrontEnd/scripts.js
@@ -0,0 +1,15 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for(let card of cards){
+  card.addEventListener('click', function(){
+    modalOverlay.classList.add('active')
+  })
+}
+
+closeModal = document.querySelector('.close-modal').addEventListener('click', 
+function(){
+  modalOverlay.classList.remove('active');
+
+})
+
