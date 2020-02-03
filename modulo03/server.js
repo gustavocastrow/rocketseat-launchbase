da041ab1274eead1771626a6776c2b08fd8feb03
gustavocastro <gustavocastrocs@gmail.com>
2020-02-03 07:08:13 -0300

    Configurando Nunjucks

--- modulo03/server.js
+++ modulo03/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.set('view engine', 'html');
+
+nunjucks.configure('views', {
+  express:server
+})
+
 server.get('/', function(req, res){
-  return res.send('Hello Launchbase!')
+  return res.render('index');
 
 })
 
