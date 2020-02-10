b2b1763a41c2b0184d9e33661379f1cafc1f29c0
gustavocastro <gustavocastrocs@gmail.com>
2020-02-10 11:20:55 -0300

    Estilizando os forms

--- modulo05/routes.js
+++ modulo05/routes.js
@@ -7,6 +7,16 @@ routes.get('/', function(req, res) {
 
 routes.get('/instructors', function(req, res) {
   return res.render('instructors/index')
+
+})
+
+routes.get('/instructors/create', function(req, res) {
+  return res.render('instructors/create')
+
+})
+
+routes.post('/instructors', function(req, res){
+  return  res.send('recebido')
 })
 
 routes.get('/members', function(req, res) {
