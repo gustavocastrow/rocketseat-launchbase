59d42c824dc366f14075905994ef735139521a8a
gustavocastro <gustavocastrocs@gmail.com>
2020-02-12 19:42:09 -0300

    Buscando instructor com parametros da rota

--- modulo05/instructors.js
+++ modulo05/instructors.js
@@ -2,6 +2,22 @@ const fs = require('fs')
 const data = require('./data.json')
 
 // função para create
+exports.show = function(req,res){
+
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return instructor.id == id
+  })
+
+  if(!foundInstructor) return res.send('Instructor not found!')
+
+
+  return res.send(foundInstructor)
+
+
+
+}
 
 exports.post = function(req, res){
   
@@ -16,10 +32,21 @@ exports.post = function(req, res){
         }
     }
 
-    // [ ] 1x
-    // [{...}] 2x
+  let { avatar_url, name, birth, gender, services, } = req.body
 
-    data.instructors.push(req.body) // [{...}]
+  birth = Date.parse(birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
+  
+  data.instructors.push({
+      id,
+      avatar_url,
+      name,
+      birth,
+      gender,
+      services,
+      created_at
+    }) 
 
     fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
       if(err) return res.send('Write file error')
--- modulo05/routes.js
+++ modulo05/routes.js
@@ -16,6 +16,8 @@ routes.get('/instructors/create', function(req, res) {
 
 })
 
+routes.get('/instructors/:id', instructors.show)
+
 routes.post('/instructors', instructors.post )
 
 
