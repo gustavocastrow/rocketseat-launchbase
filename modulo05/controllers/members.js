78faac8280066030391bb49ee7d34927cb8dbaf4
gustavocastro <gustavocastrocs@gmail.com>
2020-02-26 23:00:01 -0300

    Ajustando dados de cadastro de novo membro

--- modulo05/controllers/members.js
+++ modulo05/controllers/members.js
@@ -46,26 +46,25 @@ exports.post = function(req, res){
         }
     }
 
-  let { avatar_url, name, birth, gender, services, } = req.body
 
-  birth = Date.parse(birth)
-  const created_at = Date.now()
-  const id = Number(data.members.length + 1)
+  birth = Date.parse(req.body.birth)
+  let id = 1
+  const lastMember = data.members[data.members.length - 1]
+  
+  if(lastMember) { 
+    id = lastMember.id + 1
+  }
   
   data.members.push({
       id,
-      avatar_url,
-      name,
-      birth,
-      gender,
-      services,
-      created_at
+    ...req.body,
+      birth
     }) 
 
     fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
       if(err) return res.send('Write file error')
 
-      return res.redirect('/members')
+      return res.redirect(`/members/${id}`)
 
     })
     //return  res.send(req.body)
