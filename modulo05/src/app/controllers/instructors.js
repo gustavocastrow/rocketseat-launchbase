ee3edb5b5a58521480f5491eed379bc06a05a876
gustavocastrow <gustavocastrocs@gmail.com>
2020-04-03 09:51:02 -0300

    Correção bug paginação

--- modulo05/src/app/controllers/instructors.js
+++ modulo05/src/app/controllers/instructors.js
@@ -2,32 +2,30 @@ const { age, date } = require('../../lib/utils')
 const Instructor = require('../models/Instructor')
 
 module.exports = {
-  index(req, res) {
-    let { filter, page, limit } = req.query
-
-   page = page || 1 
-   limit = limit || 2
-   let offset = limit * (page -1)
-   
-   
-   const params = {
-     filter,
-     page,
-     limit,
-     offset,
-     callback(instructors){
-
-      const pagination = {
-        total: Math.ceil(instructors[0].total / limit),
-        page
-      }
-      return res.render("instructors/index", { instructors, pagination, filter })
-     }
-   }
-
-   Instructor.paginate(params)
-
-  },
+    index(req, res) {
+        let { filter, page, limit } = req.query
+    
+        page = page || 1
+        limit = limit || 5
+        let offset = limit * (page - 1)
+    
+        const params = {
+          filter,
+          page,
+          limit,
+          offset,
+          callback(instructors) {
+    
+            const pagination = {
+              total: Math.ceil(instructors[0].total / limit),
+              page
+            }
+            return res.render("instructors/index", { instructors, pagination, filter })
+          }
+        }
+    
+        Instructor.paginate(params)
+      },
 
 
   create(req, res) {
