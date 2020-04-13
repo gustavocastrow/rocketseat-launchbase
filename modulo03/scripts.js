7c85cfe245cb6f1be945d5a4c2cba0cec5c4cce7
gustavocastrow <gustavocastrocs@gmail.com>
2020-04-13 16:23:35 -0300

    Dynamic Video

--- modulo03/scripts.js
+++ modulo03/scripts.js
@@ -1,17 +1,17 @@
-
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card');
-
 //Ativando o modal.
 for(let card of cards){
   card.addEventListener("click", function(){
+    const videoId = card.getAttribute("id")
     modalOverlay.classList.add('active')
+    modalOverlay.querySelector('iframe').src=`https://youtube.com/embed/${videoId}`;
   });
 }
-
 //Fechando o modal.
-
 document.querySelector('.close-modal').
   addEventListener("click", function(){
     modalOverlay.classList.remove('active');
+    modalOverlay.querySelector('iframe').src="";
   })
+
