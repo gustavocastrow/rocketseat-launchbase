5d13bb19a1cfb5daf1cf046011ce1889421b5d65
gustavocastrow <gustavocastrocs@gmail.com>
2020-05-13 09:33:31 -0300

    Usando dados dinamicos

---
+++ modulo03/data.js
@@ -0,0 +1,45 @@
+module.exports = [
+  {
+    id: "SXSKvUB9wDA",
+    title: "Pagamento com stripe e NextJS ",
+    duration: "44:40min",
+    price: "FREE"
+  },
+
+  {
+    id: "5yJ_cAUrpQc",
+    title: "Eu cometi esse erro demais! ",
+    duration: "44:40min",
+    price: "FREE"
+  },
+
+  {
+    id: "U7GjS3FuSkA",
+    title: "SQL no NodeJS com KnexJS",
+    duration: "44:40min",
+    price: "FREE"
+  },
+
+  {
+    id: "BwwOu29K6mE",
+    title: "Transição de imagens com CSS e JS Puro",
+    duration: "44:40min",
+    price: "FREE"
+  },
+
+  {
+    id: "KISMYYXSIX8",
+    title: "Context API + Hooks",
+    duration: "44:40min",
+    price: "FREE"
+  },
+
+  {
+    id: "nYLd-i4KA1o",
+    title: "Melhores momentos - Carreira e mercado",
+    duration: "44:40min",
+    price: "FREE"
+  },
+
+
+]
--- modulo03/server.js
+++ modulo03/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require("./data");
 
 server.use(express.static('public'));
 
@@ -12,11 +13,23 @@ nunjucks.configure("views", {
 });
 
 server.get("/", function(req, res){
-  return res.render("about");
+  const about = {
+    avatar_url: "https://avatars0.githubusercontent.com/u/28929274?s=200&v=4",
+    name: "Rocketseat",
+    role: "A menor distancia entre você e seus objetivos",
+    description: "Transforme sua carreira e seja um programador desejado no mercado, dominando as ferramementas mais modernas de desenvolvimento web e mobile",
+    links: [
+      {name: "Site", url:"https://rocketseat.com.br/"},
+      {name: "Youtube", url:"https://www.youtube.com/rocketseat"},
+      {name: "Instagram", url:"https://www.instagram.com/rocketseat_oficial/"}
+    ]
+    
+  }
+  return res.render("about", {about});
 });
 
 server.get("/portfolio", function(req, res){
-  return res.render("portfolio");
+  return res.render("portfolio", {items: videos});
 });
 
 server.listen(5000, function(){
