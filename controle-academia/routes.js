9c455399554b0570039222ef7bb69b2b6b16e86b
Gustavo Castro <gustavocastrocs@gmail.com>
2020-09-29 14:40:32 -0300

    Style menu

--- controle-academia/routes.js
+++ controle-academia/routes.js
@@ -2,7 +2,16 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res){
-  return res.send("hello world!")
+  return res.redirect("instructors")
 });
+
+routes.get('/instructors', function(req, res){
+  return res.render("instructors/index")
+});
+
+routes.get('/members', function(req, res){
+  return res.send("members")
+});
+
 
 module.exports = routes 
