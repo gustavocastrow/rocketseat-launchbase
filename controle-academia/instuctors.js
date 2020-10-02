21699f20acc67796065fa718d207dde5384e966d
Gustavo Castro <gustavocastrocs@gmail.com>
2020-10-02 18:46:07 -0300

    Manipulando Dados

--- controle-academia/instuctors.js
+++ controle-academia/instuctors.js
@@ -1,6 +1,30 @@
 const fs = require('fs')
 const data = require("./data.json")
 
+// SHOW
+exports.show = function(req, res) {
+  //req.query.id = ?id=1
+  //req.body = recebe do formulario atraves do post
+  //req.params = recebe diretamente da url (/:id)
+  const { id } = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return instructor.id == id
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found!")
+
+  const instructor = {
+    ...foundInstructor,
+    age: "",
+    services: foundInstructor.services.split(","), 
+    created_at: "",
+  }
+
+  return res.render("instructors/show", { instructor });
+
+
+}
 // CREATE
 exports.post = function(req, res) {
   //req.query
@@ -14,11 +38,24 @@ exports.post = function(req, res) {
     }
   }
 
-  req.body.birth = Date.parse(req.body.birth)
-  req.body.created_at = Date.now()
+  let {avatar_url, birth, name, services, gender} = req.body
+
+  birth = Date.parse(birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
+
+  
 
   // [ ] .. push -> [{...}] -> [{...}, {...}]
-  data.instructors.push(req.body)
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
+  })
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
     if (err) return res.send("Write file error!")
--- controle-academia/routes.js
+++ controle-academia/routes.js
@@ -14,6 +14,8 @@ routes.get('/instructors/create', function(req, res) {
   return res.render("instructors/create")
 });
 
+routes.get('/instructors/:id', instructors.show)
+
 routes.post('/instructors', instructors.post)
 
 routes.get('/members', function(req, res){
